266c3c119b20bb91db1e4a08fcdc028c97f66e5f
Səma <mammadzadasama@gmail.com>
2025-09-02 06:47:44 +0000

    Olke və index faylları əlavə edildi

---
+++ task_1.php/olke.php
@@ -0,0 +1,12 @@
+<?php
+class Olke {
+    public $ad;
+
+        public function __construct($ad) {
+                $this->ad = $ad;
+                    }
+
+                        public function tanit() {
+                                return "Mənim ölkəm " . $this->ad . "dir.";
+                                    }
+                                    }
